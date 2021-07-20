cf7d7b9d8049abcd901fe22503520581006bdb82
Manas-Bagai <manas.bagai@razorpay.com>
2021-07-20 19:02:38 +0530

    Changed exercise 2 solution

--- day1/exercise2/solution.go
+++ day1/exercise2/solution.go
@@ -1,24 +1,35 @@
 package main
 
+import "fmt"
+
 type Tree struct{
 	Left *Tree
 	Right *Tree
-	Val int
+	Val string
 }
 
-func NodetoTree(tree *Tree, node *Tree){
-	if tree.Val== 1{
-		tree.Val=node.Val
-		return
+func (t *Tree) preorder() {
+	if t != nil {
+		fmt.Printf("%v", t.Val)
+		t.Left.preorder()
+		t.Right.preorder()
 	}
-
+}
+func (t *Tree) postorder(){
+	if t != nil {
+		t.Left.preorder()
+		t.Right.preorder()
+		fmt.Printf("%v", t.Val)
+	}
 }
 
-func TreeMak(values []int)( *Tree){
-	root:= Tree{Left: nil, Right: nil, Val: 1}
-	for _,v := range values{
-		node:= Tree{Val: v}
-		NodetoTree(&root,&node)
-	}
-	return &root
+func main(){
+	node1:=Tree{Left:nil, Right: nil, Val: "a"}
+	node2:= Tree{Left:nil, Right: nil, Val: "b"}
+	node3:= Tree{Left:nil, Right: nil, Val: "c"}
+	node4:= Tree{Left:&node2, Right: &node3, Val: "-"}
+	node5:= Tree{Left:&node1, Right: &node4, Val: "+"}
+	node5.preorder()
+	fmt.Println(" ")
+	node5.postorder()
 }
