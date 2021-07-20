9d5aa298a0f888069e13cb0599eda8f3cca40378
Manas-Bagai <manas.bagai@razorpay.com>
2021-07-20 18:19:29 +0530

    Exercise 2

--- day1/exercise2/solution.go
+++ day1/exercise2/solution.go
@@ -1,1 +1,24 @@
-package exercise2
+package main
+
+type Tree struct{
+	Left *Tree
+	Right *Tree
+	Val int
+}
+
+func NodetoTree(tree *Tree, node *Tree){
+	if tree.Val== 1{
+		tree.Val=node.Val
+		return
+	}
+
+}
+
+func TreeMak(values []int)( *Tree){
+	root:= Tree{Left: nil, Right: nil, Val: 1}
+	for _,v := range values{
+		node:= Tree{Val: v}
+		NodetoTree(&root,&node)
+	}
+	return &root
+}
