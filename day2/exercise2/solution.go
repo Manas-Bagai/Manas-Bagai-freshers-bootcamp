3009e3c3fe177a2bb060d6f33baf98d24d416b15
Manas-Bagai <manas.bagai@razorpay.com>
2021-07-22 17:11:10 +0530

    solution 2 added

--- day2/exercise2/solution.go
+++ day2/exercise2/solution.go
@@ -1,1 +1,29 @@
-package exercise2
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+
+func getGrade(wg *sync.WaitGroup, grade chan<-int){
+	defer wg.Done()
+	a:=rand.Intn(10)
+	grade<-a
+}
+
+func main(){
+	var wg sync.WaitGroup
+	grade:=make(chan int)
+	numStudents:=200
+	final:=make(map[int]int)
+	for i:=1;i<=numStudents;i++{
+		c:=rand.Intn(5)
+		wg.Add(c)
+		go getGrade(&wg,grade)
+		v:=<-grade
+		final[i]=v
+	}
+	fmt.Println(final)
+}
