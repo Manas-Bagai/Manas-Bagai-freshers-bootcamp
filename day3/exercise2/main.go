989068059ed56e938ec8004b978e7def1da2c9c0
Manas-Bagai <manas.bagai@razorpay.com>
2021-07-26 10:33:26 +0530

    day 3 exercises added

---
+++ day3/exercise2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"exercise2/Config"
+	"exercise2/Models"
+	"exercise2/Routes"
+	"fmt"
+	_"github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main(){
+	Config.DB, err= gorm.Open("mysql",Config.DbURL(Config.BuildDBConfig()))
+	if err!= nil{
+		fmt.Println("Status:", err)
+
+	}
+
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+
+	r:= Routes.SetupRouter()
+	r.Run()
+}
